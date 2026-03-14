c370e2ddca307a059cc0a3fc89092eb70fafb9ef
hayfa safa <haifasafaa@gmail.com>
2026-03-14 21:27:18 +0100

    build the Header section

---
+++ recipes-app/src/components/header/header.jsx
@@ -0,0 +1,19 @@
+
+import Logo from "./Logo.jpg"
+
+
+export default function Header () {
+  return (
+<header>
+  <nav>
+    <img src={Logo} alt="logo empgie for smile chef " />
+    <ul>
+      <li>Chef Claude</li>
+    </ul>
+  </nav>
+</header>
+
+
+
+  )
+}
