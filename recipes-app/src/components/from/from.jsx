f009cabcf171520ad9bf51f93cbd4a517feb2665
hayfa safa <haifasafaa@gmail.com>
2026-03-15 01:59:31 +0100

    add the items in the input

--- recipes-app/src/components/from/from.jsx
+++ recipes-app/src/components/from/from.jsx
@@ -1,16 +1,34 @@
 import "./from.css"
-
+ 
 
 export default function From() {
+  const ingredients = ["chicken", "orange", "apple"]
+
+const addIngredients =ingredients.map((items) => {
+  return <li key={items}>{items}</li>
+})
+
+function handleSubmit(event){
+  event.preventDefault()
+ const formData = new FormData(event.currentTarget)
+ const newIngredient = formData.get("in")
+  ingredients.push(newIngredient)
+  console.log(ingredients)
+}
+
   return (
     <main> 
-    <form className="addIngre">
+    <form  onSubmit={handleSubmit}    className="addIngre">
       <input aria-label="Add ingredient"
       type="text" 
       placeholder="e.g. oregano"
+      name="in"
       />
-      <button>Add ingredient</button>
+      <button> + Add ingredient</button>
     </form>
+    <ul>
+      {addIngredients}
+    </ul>
     </main>
   )
 }
--- recipes-app/src/components/header/header.jsx
+++ recipes-app/src/components/header/header.jsx
@@ -7,7 +7,7 @@ export default function Header () {
   return (
 <header className="navDesign">
     <img className="logo"  src={Logo} alt="logo empgie for smile chef " /> 
-      <h1 className="logoTitle">Chef Claude</h1>
+      <h1 className="logoTitle">Chef Hayfa</h1>
 </header>
 
 
